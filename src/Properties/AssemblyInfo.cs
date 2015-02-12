ca745e9f151d717d757567e7ea53651a2b118d83
William Gross <william@wgross.net>
2015-02-12 18:30:55 +0500

    Added NuGet package information.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MimeTypeMap")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "Huge dictionary of file extensions to mime types" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany( "Samuel Neff" )]
 [assembly: AssemblyProduct("MimeTypeMap")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
