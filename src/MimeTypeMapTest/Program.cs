2a0ee14a3a27f01aecc63695b134cd6d2089e6da
samuelneff <github@serndesign.com>
2015-08-14 09:33:30 -0400

    Add efficient implementation of GetExtension to get an extension from a mime type with optional deterministic mappings for duplicate mime types

--- src/MimeTypeMapTest/Program.cs
+++ src/MimeTypeMapTest/Program.cs
@@ -8,6 +8,7 @@ namespace MimeTypeMapTest
         static void Main()
         {
             Console.WriteLine("txt -> " + MimeTypeMap.GetMimeType("txt"));
+            Console.WriteLine("audio/wav -> " + MimeTypeMap.GetExtension("audio/wav"));
         }
     }
 }
