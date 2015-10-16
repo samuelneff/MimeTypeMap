76d0770fdbb2a8b7c004ab902eefdb5b6adea65b
Samuel Wilde <samuelwilde@hotmail.com>
2015-10-16 09:41:08 +0100

    Added GetKnownExtensions, which gets all known extensions for a given mime type.

--- src/MimeTypeMapTest/Program.cs
+++ src/MimeTypeMapTest/Program.cs
@@ -3,12 +3,15 @@ using MimeTypes;
 
 namespace MimeTypeMapTest
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             Console.WriteLine("txt -> " + MimeTypeMap.GetMimeType("txt"));
             Console.WriteLine("audio/wav -> " + MimeTypeMap.GetExtension("audio/wav"));
+            Console.WriteLine("Extensions with text/plain mime type: " + string.Join(", ", MimeTypeMap.GetKnownExtensions("text/plain")));
+
+            Console.ReadKey();
         }
     }
 }
